801970c18cee5ba0a6a68ac4e84b5710a7d30265
Athan Clark <athan.clark@gmail.com>
2023-12-20 05:13:16 -0500

    minimum of one step

--- public/client.js
+++ public/client.js
@@ -1,8 +1,42 @@
 import $ from 'jquery';
 
 $(document).ready(() => {
-    $('#new-task button').on('click', function(e) {
+    $('#new-task > button').on('click', e => {
         e.preventDefault();
+    });
+    // $('#new-task tbody button').on('click', e => {
+    //     e.preventDefault();
+    // })
+    $('#new-task > a').on('click', e => {
+        e.preventDefault();
+        const steps_length = $('#new-task tbody').children().length;
+        function makeDeleteButton(idx) {
+            return $('<button></button>').text('Delete').on('click', e => {
+                e.preventDefault();
+                $('#new-task tbody').children().eq(steps_length).remove();
+                if ($('#new-task tbody').children().length === 1) {
+                    $('#new-task tbody > tr > td:last-child > button').remove();
+                }
+            });
+        }
+        if (steps_length === 1) {
+            $('#new-task tbody > tr > td:last-child').append(
+                makeDeleteButton(0)
+            );
+        }
+        $('#new-task tbody').append(
+            $('<tr></tr>').append([
+                $('<td></td>').append(
+                    $('<input></input>').attr('type','text')
+                ),
+                $('<td></td>').append(
+                    $('<textarea></textarea>')
+                ),
+                $('<td></td>').append(
+                    makeDeleteButton(steps_length)
+                )
+            ])
+        );
     });
 
     // gets initial data
